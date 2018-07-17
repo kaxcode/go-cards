c65968a91d38f6bfb9d9143bc1c455fd14db643d
Kenia <keniavalladarez@gmail.com>
2018-07-16 21:47:47 -0400

    Variable declaration example.

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	// only use := to define new variables
+	card := "Aces of Spades"
+	// reassign value of variable using = not :=
+	card = "Five of Diamonds"
+	fmt.Println(card)
 }
