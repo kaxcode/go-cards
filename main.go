d85e5bad8e15a987303cbcb74c1ca77ed186979f
Kenia <keniavalladarez@gmail.com>
2018-07-24 10:33:34 -0400

    Refactoring to use the deck typ and it's print () method.

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	// iterating and printing each card
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
