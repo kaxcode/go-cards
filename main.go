84bcc10a7492a8fc75d55afba5eeb0db9819fee3
Kenia <keniavalladarez@gmail.com>
2018-07-30 09:09:15 -0400

    Prints out the string of all the cards in deck.

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	fmt.Println(cards.toString())
 
-	hand.print()
-	remainingCards.print()
 }
