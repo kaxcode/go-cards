2f4ff3f4745134db40a20a9cd872f1b02408eb26
Kenia <keniavalladarez@gmail.com>
2018-08-03 11:27:12 -0400

    Adds test for checking what type of card is first in deck.

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,8 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 20 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but go %v", d[0])
+	}
 }
